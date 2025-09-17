9fbedfdede20eb4b26f29c449a4dc7aaffd159bb
NataliaDelgado <natdelcas143@gmail.com>
2025-09-17 07:38:22 -0600

    comentarios

---
+++ Launidad2/IntroduccionAPython/comentarios.py
@@ -0,0 +1,13 @@
+#Este programa imprime la variable. También observamos la mutabilidad de python
+
+entero = 10
+flotante = 3.14
+cadena = "Nat Delgado"
+booleana = True
+
+print(entero)
+print(flotante)
+print(cadena)
+print(booleana)
+#Edd Flores
+#ITPA
