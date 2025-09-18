31b5cf0e7cec26e0717810e46e8a289b62fffc3e
NataliaDelgado <natdelcas143@gmail.com>
2025-09-18 07:58:48 -0600

    Lista

---
+++ Launidad2/IntroduccionAPython/Lista.py
@@ -0,0 +1,14 @@
+#Nombrando las variables
+Lista=["Sinai","Leo","Fatima","Chris"]
+print (Lista)
+#Seleccionando la variable de la lista
+print (Lista[2])
+#Agregando un elemento
+Lista.append("Alexa")
+print (Lista)
+Lista.insert(1,"Luis")
+print(Lista)
+#Insertando una de las variables
+Lista.pop()
+Lista.remove("Sinai")
+Lista.pop(1)
